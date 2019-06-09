619b30afc254264b3ec9483fa5315e4fd197b1e4
Tim <t.mouskhelichvili@gmail.com>
2019-06-09 16:01:42 -0400

    Fixed build

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
